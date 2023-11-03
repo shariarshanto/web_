fb88cd3113932a5ddb74b907f2cc549ac979be5f
sharier <shantohmmm@gmail.com>
2023-11-03 20:44:10 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,8 @@
+<script>
+  
+        var base   = parseFloat(prompt("Please enter your Base: "));
+        var height = parseFloat(prompt("Please enter your Height: "));
+        var area   = (base * height)/2;
+        document ("Area: " + area);
+
+ </script>
